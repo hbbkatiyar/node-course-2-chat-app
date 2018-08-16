42f005f1460e4967ce9713185accad721fdb3788
binit katiyar <binit.katiyar@gmail.com>
2018-08-17 00:14:19 +0530

    Add connect and disconnect

--- server/server.js
+++ server/server.js
@@ -1,18 +1,31 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
-const app = express();
 const port = process.env.PORT || 3000;
+const app = express();
+// Earlier we were running server using app.listen
+// Now we have to incorporate socket.io with oor web Server
+// This can be done as follows
+var server = http.createServer(app);
+var io = socketIO(server);
 
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
+});
+
+// app.user(express.static(__dirname + '/../public'))
+// OR
 app.use(express.static(publicPath));
 
-// app.get('/', (req, res) => {
-//   res.render('home.html');
-// });
-
-app.listen(port, () => {
+// Here express uses a build-in http module to create a web server
+// Here app.listen means http.createServer internally
+server.listen(port, () => {
   console.log(`Server is up on port: ${port}`);
 });
-//console.log(__dirname + '/../public');
-//console.log(publicPath);
