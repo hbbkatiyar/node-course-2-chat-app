3d9d65972b943de2a9f3ac49e61c972032ba4ba4
binit katiyar <binit.katiyar@gmail.com>
2018-08-26 12:42:08 +0530

    started adding emoji

--- public/js/chat.js
+++ public/js/chat.js
@@ -77,7 +77,8 @@ jQuery('#message-form').on('submit', function (e) {
   var messageTextBox = jQuery('[name=message]');
 
   socket.emit('createMessage', {
-    text: messageTextBox.val()
+    text: _showEmoji(messageTextBox.val())
+    //text: messageTextBox.val()
   }, function() {
     messageTextBox.val('')
   });
@@ -103,3 +104,63 @@ locationButton.on('click', function () {
     locationButton.removeAttr('disabled').text('Sending location');
   })
 });
+
+var _showEmoji = function(msg) {
+  return msg;
+};
+
+/***** Start: Emoji Section ****/
+// var _initialEmoji = function() {
+//     var emojiContainer = document.getElementById('emojiWrapper'),
+//         docFragment = document.createDocumentFragment();
+//     for (var i = 69; i > 0; i--) {
+//         var emojiItem = document.createElement('img');
+//         emojiItem.src = '../content/emoji/' + i + '.gif';
+//         emojiItem.title = i;
+//         docFragment.appendChild(emojiItem);
+//     };
+//     emojiContainer.appendChild(docFragment);
+// };
+// var _showEmoji = function(msg) {
+//     var match, result = msg,
+//         reg = /\[emoji:\d+\]/g,
+//         emojiIndex,
+//         totalEmojiNum = document.getElementById('emojiWrapper').children.length;
+//
+//     while (match = reg.exec(msg)) {
+//         emojiIndex = match[0].slice(7, -1);
+//         if (emojiIndex > totalEmojiNum) {
+//             result = result.replace(match[0], '[X]');
+//         } else {
+//             result = result.replace(match[0], '<img class="emoji" src="../content/emoji/' + emojiIndex + '.gif" />');//todo:fix this in chrome it will cause a new request for the image
+//         };
+//     };
+//     return result;
+// };
+//
+// document.getElementById('emoji').addEventListener('click', function(e) {
+//     var emojiwrapper = document.getElementById('emojiWrapper');
+//     emojiwrapper.style.display = 'block';
+//     e.stopPropagation();
+// }, false);
+//
+// document.body.addEventListener('click', function(e) {
+//     var emojiwrapper = document.getElementById('emojiWrapper');
+//     if (e.target != emojiwrapper) {
+//         emojiwrapper.style.display = 'none';
+//     };
+// });
+//
+// document.getElementById('emojiWrapper').addEventListener('click', function(e) {
+//     var target = e.target;
+//     if (target.nodeName.toLowerCase() == 'img') {
+//         var messageInput = document.getElementById('messageInput');
+//         messageInput.focus();
+//         messageInput.value = messageInput.value + '[emoji:' + target.title + ']';
+//     };
+// }, false);
+//
+// jQuery(document).ready(function() {
+//   _initialEmoji();
+// });
+/***** End: Emoji Section *****/
